1c486c3bd99432ef696ca44966f1a03963a37c79
alikushbaev <149851671+alikushbaev@users.noreply.github.com>
2025-03-27 17:41:59 +0500

    Add files via upload

---
+++ gg.py
@@ -0,0 +1,39 @@
+# https://www.youtube.com/@alikushbaev3
+
+# print(str("Text"))
+# print(str(1))
+# print(int(1))
+
+# ValueError: invalid literal for int() with base 10: 'Text'
+# print(int("Text"))
+
+# print(int(1.2))
+
+# print(float(1.122))
+
+# ValueError: could not convert string to float: 'Text'
+# print(float("Text"))
+
+# False
+# print(bool(0))
+# True
+# print(bool(1)) # До бесканечносьть
+
+# True
+# print(bool(-2))
+
+# Практика
+
+# try: 
+#     a= int(input("Сколько Тебе Лет? "))
+# except:
+#     print("Надо Цыфро а не Буквы")
+#     a= int(input("Сколько Тебе Лет? "))
+# print(a)
+
+# a= str(input("Как Тебя Завут? "))
+# print(a)
+
+# a=float(input("Пишите Номер Например: 1.3: "))
+# print(a)
+
